e907755516cc3500dcda449858ce9dbf810d070e
Pedro Arantes <arantespp@gmail.com>
2019-07-20 15:31:02 -0300

    Updates

---
+++ precache-manifest.2b676a77044d0161b829a66e1fcae95c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c27318ba20086b408be64e4a87c79cc",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "79494b81a5caf593fb75",
+    "url": "/my-react-app/static/css/main.16bfbd37.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/my-react-app/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "79494b81a5caf593fb75",
+    "url": "/my-react-app/static/js/main.e2cf4f35.chunk.js"
+  },
+  {
+    "revision": "b28cb091c153ab1cf392",
+    "url": "/my-react-app/static/js/runtime~main.f5ab8794.js"
+  }
+]);
